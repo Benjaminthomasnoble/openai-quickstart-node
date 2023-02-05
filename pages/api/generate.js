5fd6be2bd42c45142a70851c34d21e4c6b36e45d
Ben Noble <benjamin4504@gmail.com>
2023-02-05 13:34:40 -0500

    first commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -14,7 +14,7 @@ export default async function (req, res) {
     });
     return;
   }
-
+  v
   const animal = req.body.animal || '';
   if (animal.trim().length === 0) {
     res.status(400).json({
@@ -32,7 +32,7 @@ export default async function (req, res) {
       temperature: 0.6,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
